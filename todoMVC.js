1ef5a29f6f30b4b478a54534acd70daaa5c1139f
zoe <2577213154@qq.com>
2020-08-21 22:46:57 +0800

    2020/08/21 周五 22:46:57.18

--- todoMVC.js
+++ todoMVC.js
@@ -1,154 +1,142 @@
-//重新打开页面，也要显示之前的,读取local storage
-$().ready(function () {
-  for (let i = 0; ; i++) {
-    if (!localStorage[i]) {
-      continue;
-    }
-    let newLi = `<li id ='${i}'><input type ="checkbox"></input>${
-      localStorage[i]
-    }<button id='${"btn" + i}' class="not-hover"></button>
-		<label for='${"btn" + i}' class="not-hover">×</label></li>`;
-    $("ul").prepend(newLi);
-    let li = $("ul")[0].firstElementChild;
-    li.addEventListener("mouseover", (e) => {
-      li.lastElementChild.classList.remove("not-hover");
-      li.lastElementChild.classList.add("del-label");
-    });
-    li.addEventListener("mouseout", (e) => {
-      li.lastElementChild.classList.remove("del-label");
-      li.lastElementChild.classList.add("not-hover");
-    });
-    if (
-      Array.from(document.getElementsByTagName("li")).length >=
-      localStorage.length
-    ) {
-      break;
-    }
+let todoInput = document.querySelector("#todoInput");
+let todoList = document.querySelector("#todoList");
+let toggleAllInput = document.querySelector("#all");
+let stateWarp = document.querySelector(".state");
+let leftCount = document.querySelector(".left-count");
+let clearCompletedBtn = document.querySelector("#clear-completed");
+let categorySelect = document.querySelector("#category-select");
+categorySelect.onclick = (e) => {
+  //筛选活动的，完成的，还是全部
+  if (e.target.matches("input")) {
+    todoList.classList.remove("all", "active", "completed");
+    todoList.classList.add(e.target.value);
   }
-});
-window.onload = (e) => {
-  let newTodo = $(".new")[0];
-  newTodo.addEventListener("keydown", (e) => {
-    if (e.code === "Enter" && e.target.value !== "") {
-      addToList(e.target.value);
-      e.target.value = "";
-    }
+};
+toggleAllInput.onclick = (e) => {
+  let completed = todoList.querySelectorAll("li.completed");
+  if (completed.length === todoList.children.length) {
+    //全被选中了
+    Array.from(todoList.children).forEach((it) => {
+      it.classList.remove("completed");
+      it.firstElementChild.checked = false;
+    });
+    setSelectAllAndLeftCountAndClearBtn();
+  } else {
+    Array.from(todoList.children).forEach((it) => {
+      it.classList.add("completed");
+      it.firstElementChild.checked = true;
+    });
+    setSelectAllAndLeftCountAndClearBtn();
+  }
+};
+todoInput.onkeyup = function (e) {
+  //添加了新的todo,在下面创建一个结构
+  if (e.key === "Enter") {
+    addTodo({ content: e.target.value.trim(), completed: false });
+    e.target.value = ''
+    save()
+  }
+};
+clearCompletedBtn.onclick = (e) => {
+  //清楚已完成的，清除完这个按钮消失
+  let toDeleted = document.querySelectorAll(".completed");
+  toDeleted.forEach((it) => {
+    todoList.removeChild(it);
   });
+  clearCompletedBtn.style.display = "none";
+};
 
-  function addToList(value) {
-    let i = $("ul")[0].children.length
-      ? +$("ul")[0].firstElementChild.id + 1
-      : 0;
-    window.localStorage.setItem(i, value);
-    let newLi = `<li id ='${i}'>
-		<input type ="checkbox"></input>
-		${value}
-		<button id='${"btn" + i}' class="not-hover"></button>
-		<label for='${"btn" + i++}' class="not-hover">×</label>
-		</li>`;
-    $("ul").prepend(newLi);
-    let li = $("ul")[0].firstElementChild;
-    li.addEventListener("mouseover", (e) => {
-      li.lastElementChild.classList.remove("not-hover");
-      li.lastElementChild.classList.add("del-label");
-    });
-    li.addEventListener("mouseout", (e) => {
-      li.lastElementChild.classList.remove("del-label");
-      li.lastElementChild.classList.add("not-hover");
-    });
-    //添加了保存在local storage,key('0','1'...)
+function setSelectAllAndLeftCountAndClearBtn() {
+  //判断是不是全被选了
+  let actives = todoList.querySelectorAll("input:first-child:not(:checked)");
+  let completed = todoList.querySelectorAll("input:first-child:checked");
+  if (actives.length) {
+    toggleAllInput.checked = false;
+  } else {
+    toggleAllInput.checked = true;
   }
-
-  let all = $("#forall")[0];
-  let lis = Array.from(document.getElementsByTagName("li"));
-
-  for (let li of lis) {
-    li.addEventListener("mouseover", (e) => {
-      li.lastElementChild.classList.remove("not-hover");
-      li.lastElementChild.classList.add("del-label");
-    });
-    li.addEventListener("mouseout", (e) => {
-      li.lastElementChild.classList.remove("del-label");
-      li.lastElementChild.classList.add("not-hover");
-    });
+  leftCount.textContent = actives.length + "items left";
+  if (completed.length) {
+    clearCompletedBtn.style.display = "inline-block";
+  } else {
+    clearCompletedBtn.style.display = "none";
   }
-  function isAllSelected() {
-    let lis = Array.from(document.getElementsByTagName("li"));
-    for (let i = 0; i < lis.length; i++) {
-      if (lis[i].firstElementChild.checked == false) {
-        return false;
-      }
-    }
-    return true;
-  }
-  //如果全选中了，下一次点击是取消全选;否则点击时全选
-  //TODO 可能有问题
-  window.onload(e) = (e) => {
-    console.log(lis.length);
-    all.onclick = (e) => {
-      if (isAllSelected()) {
-        console.log(lis.length);
-        for (let i = 0; i < lis.length; i++) {
-          lis[i].firstElementChild.checked = false;
-          all.classList.remove("selected-all");
-          all.classList.add("selected-none");
-          lis[i].classList.remove("bechecked");
-        }
+}
+let todos = JSON.parse(localStorage.todos) || [];
+todos.forEach((todo) => {
+  addTodo(todo);
+});
+function addTodo(todo) {
+  let todoText = todo.content;
+  if (todoText.trim() !== "") {
+    this.value = "";
+    let li = document.createElement("li");
+    li.classList.add("todo-item");
+    todo.completed && li.classList.add("completed");
+    let checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.checked = todo.completed;
+    checkbox.onchange = (e) => {
+      //这一项是否完成
+      if (checkbox.checked) {
+        li.classList.add("completed");
       } else {
-        for (let i = 0; i < lis.length; i++) {
-          lis[i].firstElementChild.checked = true;
-          all.classList.remove("selected-none");
-          all.classList.add("selected-all");
-          lis[i].classList.add("bechecked");
-        }
+        li.classList.remove("completed");
+      }
+      setSelectAllAndLeftCountAndClearBtn();
+      save()
+    };
+
+    let span = document.createElement("span");
+    span.textContent = todoText;
+    span.ondblclick = (e) => {
+      //编辑这条todo
+      li.classList.add("editing");
+      setTimeout(() => {
+        editBox.focus();
+      });
+    };
+    let editBox = document.createElement("input"); //默认不显示，编辑这条的时候才显示
+    editBox.type = "text";
+    editBox.value = todoText;
+    editBox.onkeyup = (e) => {
+      //编辑这条todo，编辑完了
+      if (e.key === "Enter") {
+        span.textContent = editBox.value;
+        li.classList.remove("editing");
       }
     };
-  };
-  $("ul")[0].addEventListener("change", (e) => {
-    console.log("ul change");
-    if (isAllSelected()) {
-      //如果全选中了，上面的变成红色；否则上面的不变色
-      all.classList.remove("selected-none");
-      all.classList.add("selected-all");
-    } else {
-      all.classList.remove("selected-all");
-      all.classList.add("selected-none");
-    }
-    for (let i = 0; i < lis.length; i++) {
-      //监听每个checkbox,被选中了，添加删除线
-      if (lis[i].firstElementChild.checked == true) {
-        lis[i].classList.add("bechecked");
-      } else {
-        lis[i].classList.remove("bechecked");
-      }
-    }
-  });
+    editBox.onblur = (e) => {
+      span.textContent = editBox.value;
+      li.classList.remove("editing");
+    };
 
-  //TODO 没有old隐藏上面的all
-  //button,删元素，删local Storage
-  $("ul")[0].addEventListener("click", (e) => {
-    if (e.target.matches(".del-label")) {
-      localStorage.removeItem(e.target.parentNode.id);
-      e.target.parentNode.remove();
-    }
-  });
-};
-function howManyLeft() {
-  let count = 0;
-  for (let li of Array.from(document.getElementsByTagName("li"))) {
-    if (li.firstElementChild.checked == false) {
-      count++;
-    }
+    let deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "x";
+    deleteBtn.onclick = (e) => {
+      //点个X
+      todoList.removeChild(li);
+      setSelectAllAndLeftCountAndClearBtn();
+
+      if (!todoList.children.length) {
+        //todo列表没有东西，隐藏下面state
+        stateWarp.style.display = "none";
+      }
+      save()
+    };
+    li.append(checkbox, span, editBox, deleteBtn);
+    todoList.append(li);
+    stateWarp.style.display = "block";
+    setSelectAllAndLeftCountAndClearBtn();
   }
-  return count;
 }
-window.onload = (e) => {
-  //ul footer 还剩几个的span
-  let count = howManyLeft(); //TODO count要重新计算
-  let span = `<span>${count}item left</span>`;
-  $(".ul-footer").append(span);
-};
-$("ul")[0].onchange = (e) => {
-  let count = howManyLeft();
-  $(".ul-footer>span")[0].innerHTML = `<span>${count}item left</span>`;
-};
+function save() {
+  //存在localstorage里面
+  let todos = Array.from(todoList.children).map((li) => {
+    return {
+      completed: li.firstElementChild.checked,
+      content: li.firstElementChild.nextElementSibling.textContent,
+    };
+  });
+  localStorage.todos = JSON.stringify(todos);
+}
