baaa59a089a87df4734d3c4fd2e941c5c21d5f89
zoe <2577213154@qq.com>
2020-09-04 17:23:51 +0800

    add render-props

--- React/learn-tcaer/src/index.js
+++ React/learn-tcaer/src/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { Component,useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
@@ -22,6 +22,3 @@ function Greeting(props) {
 
 ReactDOM.render(<Greeting />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
