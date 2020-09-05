c63b17b1e58afcfb67ecf9365309cb32edc3335b
zoe <2577213154@qq.com>
2020-09-05 17:59:55 +0800

    update .gitignore

---
+++ git-push-today.bat
@@ -0,0 +1,14 @@
+cd /d e:\miao
+git add .
+git commit -a -m "%date% %time%"
+git push
+
+cd e://notes
+git add .
+git commit -a -m "%date% %time%"
+git push
+
+cd e://LeetCode
+git add .
+git commit -a -m "%date% %time%"
+git push
