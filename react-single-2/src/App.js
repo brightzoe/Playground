11f0ec537c1da4ac82ab5a997cafd87c6c6fa19f
zoe <2577213154@qq.com>
2020-09-06 17:53:26 +0800

    add single-page todo

--- react-single-2/src/App.js
+++ react-single-2/src/App.js
@@ -1,13 +1,38 @@
 import React from 'react';
 import './App.scss';
+class App extends React.Component {
+	state = {
+		val: '',
+		list: [],
+	};
+	handleAdd = () => {
+		const { val, list } = this.state; //解析赋值
+		list.push(val);
+		this.setState({
+			list,
+		});
+	};
+	handleChange = (e) => {
+		let val = e.target.value;
+		this.setState({ val });
+	};
+	render() {
+		const { val, list } = this.state; //解析赋值
+		return (
+			<div>
+				<p>欢迎！</p>
+				<h1>hello</h1>
+				<input type="text" value={val} onChange={this.handleChange} />
+				<button onClick={this.handleAdd}>添加</button>
 
-function App() {
-	return (
-    <div>
-      <p>欢迎！</p>
-			<h1>hello</h1>
-		</div>
-	);
+				<ul>
+					{list.map((item, idx) => {
+						return <li key={idx}>{item}</li>;
+					})}
+				</ul>
+			</div>
+		);
+	}
 }
 
 export default App;
