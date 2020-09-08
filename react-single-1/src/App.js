79dc9b6b0b24da6e6a8b917fe4cce089ed2585c7
zoe <2577213154@qq.com>
2020-09-08 09:38:10 +0800

    del single1

--- react-single-1/src/App.js
+++ react-single-1/src/App.js
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+	const [count, setCount] = useState(0); //描述初始状态
+	const [obj, setObj] = useState({ name: 'haha' });
+	return (
+		<div>
+			Your count:{count}
+			<button onClick={setCount(count + 1)}>Add</button>
+			<h2>{obj.name}</h2>
+		</div>
+	);
 }
-
-export default App;
