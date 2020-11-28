d74f41d42834b2ed984e5b10ab345151b078098a
zoe <2577213154@qq.com>
2020-11-28 22:46:56 +0800

    2020/11/28 周六 22:46:56.72

--- React/react-principle/src/react-dom.js
+++
@@ -1,9 +0,0 @@
-import {initVNode} from './vdom'
-//vnode=>dom
-//虚拟dom到真实dom树
-function render(vnode, container) {
-  const node = initVNode(vnode)
-  container.appendChild(node)
-  // container.innerHTML=`<pre>${JSON.stringify(vnode)}</pre>`
-}
-export default { render }
